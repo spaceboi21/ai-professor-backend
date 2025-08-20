7dee611f365371faa9bc5c3548c397b04b28224b
anushka-gujar23 <anushka@techtic.agency>
2025-08-20 14:41:41 +0530

    Revert "Add TranslationService for multi-language support in activity logs"
    This reverts commit a2975a560bbb63491a7da81262c70acdb0dd85c6.

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,13 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { ErrorMessageService } from './services/error-message.service';
 import { EmailEncryptionService } from './services/email-encryption.service';
-import { TranslationService } from './services/translation.service';
 import { UtilsModule } from './utils/utils.module';
 
 @Global()
 @Module({
   imports: [UtilsModule],
-  providers: [ErrorMessageService, EmailEncryptionService, TranslationService],
-  exports: [ErrorMessageService, EmailEncryptionService, TranslationService],
+  providers: [ErrorMessageService, EmailEncryptionService],
+  exports: [ErrorMessageService, EmailEncryptionService],
 })
 export class CommonModule {}
